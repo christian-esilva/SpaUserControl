6d4b6066274e87fb2e59cbd5c9ba3ed518358cca
Christian Silva <christian.eds@hotmail.com>
2019-03-19 01:54:37 -0300

    add userservice

--- SpaUserControl.Startup/DependencyResolver.cs
+++ SpaUserControl.Startup/DependencyResolver.cs
@@ -1,4 +1,6 @@
-﻿using SpaUserControl.Domain.Contracts.Repositories;
+﻿using SpaUserControl.Business.Services;
+using SpaUserControl.Domain.Contracts.Repositories;
+using SpaUserControl.Domain.Contracts.Services;
 using SpaUserControl.Domain.Entities;
 using SpaUserControl.Infrastructure.Data;
 using SpaUserControl.Repositories.Infrastructure;
@@ -13,7 +15,7 @@ namespace SpaUserControl.Startup
         {
             container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
             container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
-            //container.RegisterType<IUserService, UserService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserService, UserService>(new HierarchicalLifetimeManager());
 
             container.RegisterType<User, User>(new HierarchicalLifetimeManager());
         }
